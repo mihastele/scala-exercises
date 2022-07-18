f169c6500e8344b993c367da63f883476572d5bf
Miha Stele <stele1000@gmail.com>
2022-07-18 18:09:19 +0200

    Test commit - username email setup on local PC

--- src/main/scala/functionalP/AnonymousFunctions.scala
+++ src/main/scala/functionalP/AnonymousFunctions.scala
@@ -12,4 +12,10 @@ object AnonymousFunctions extends App {
   // anonymous function in functional way (LAMBDA) Lambda calculating
   val doublerFunc = (x: Int) => x * 2
   val doublerFunc1: Int => Int = x => x * 2 // we don't need (x: Int) here since it gets matched by compiler from definition
+
+  val adder: (Int, Int) => Int = (a: Int, b: Int) => a + b
+
+  val justDoSomething = () => println("Hello :)")
+
+  justDoSomething()
 }
